8268a1ef5cfaa30cae89b8e794273b09fdc78ed4
shashank agrawal <agrawalshashank51@gmail.com>
2026-06-04 08:11:04 +0530

    trigger image field

---
+++ DB/prisma/migrations/20260109051912_image_added/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "AvailiableAction" ADD COLUMN     "image" TEXT NOT NULL DEFAULT '';
+
+-- AlterTable
+ALTER TABLE "AvailiableTrigger" ADD COLUMN     "image" TEXT NOT NULL DEFAULT '';
